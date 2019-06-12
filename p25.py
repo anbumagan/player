4bab8b04072582c60a25d434d4ae420296a9605b
anbumagan <50615722+anbumagan@users.noreply.github.com>
2019-06-12 05:54:04 -0700

    Update p25.py

--- p25.py
+++ p25.py
@@ -1,3 +1,7 @@
 x=input()
 l=list(map(str,input().split()))
-print(*(sorted(l)))
+y=sorted(l,key=len)
+for i in range(len(y)-1):
+    if(len(y[i])==len(y[i+1]) and y[i]>y[i+1]):
+        y[i],y[i+1]=y[i+1],y[i]
+print(*y)
