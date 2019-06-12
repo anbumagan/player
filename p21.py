ac9472c40c4637e6640357eba821a42fb68f4b7e
anbumagan <50615722+anbumagan@users.noreply.github.com>
2019-06-12 04:57:12 -0700

    Update p21.py

--- p21.py
+++ p21.py
@@ -1,3 +1,4 @@
+
 a,b=map(int,input().split())
 c,d=map(int,input().split())
 e,f=map(int,input().split())
@@ -5,6 +6,7 @@ if(a==c and a==e and c==e):
     print("yes")
 elif(b==d and b==f and d==f):
     print("yes")
+elif(a==b and c==d and e==f):
+    print("yes")
 else:
     print("no")
-       
