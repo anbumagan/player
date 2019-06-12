bdc1905244fcba389db1171d556338b18da60623
anbumagan <50615722+anbumagan@users.noreply.github.com>
2019-06-12 04:31:20 -0700

    Update p20.py

--- p20.py
+++ p20.py
@@ -2,8 +2,7 @@ import numpy
 st=str(input())
 s=list(st)
 l=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-g2=numpy.roll(l,3)
-g1=list(g2)
+g1=['X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W']
 for i in st:
     if(i in g1):
         y=g1.index(i)
