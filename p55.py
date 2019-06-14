bc5f6f19a73a4a291b4a83cf732028cb57f987f5
anbumagan <50615722+anbumagan@users.noreply.github.com>
2019-06-14 02:31:36 -0700

    Create p55.py

---
+++ p55.py
@@ -0,0 +1,14 @@
+x,y=map(str,input().split())
+a=x.lower()
+b=y.lower()
+if(len(a)==len(b)):
+    c=len(a)
+    for i in range(0,len(a)):
+        if(a[i]==b[i]):
+            c=c-1
+    if(c==0):
+        print("yes")
+    else:
+        print("no")
+else:
+    print("no")
