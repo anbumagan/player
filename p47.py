feb218cc0f806571922bd8b0816e6976a9ce0c82
anbumagan <50615722+anbumagan@users.noreply.github.com>
2019-06-14 01:19:27 -0700

    Update p47.py

--- p47.py
+++ p47.py
@@ -1,5 +1,9 @@
 x,z,y=map(int,input().split())
-if((x+y+z)==180):
-    print("yes")
+if(x!=0 and y!=0 and z!=0):
+    if((x+y+z)==180):
+        print("yes")
+    else:
+        print("no")
 else:
     print("no")
+
