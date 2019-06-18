084972052dd9919ffdfafdf6f01b7fc6c0542b05
anbumagan <50615722+anbumagan@users.noreply.github.com>
2019-06-18 15:12:41 +0530

    Update p101.py

--- p101.py
+++ p101.py
@@ -1,7 +1,7 @@
 a=int(input())
 l=list(map(int,input().split()))
-sum=0
+c=[]
 for i in range(a-1):
     if(l[i]<l[i+1]):
-        sum=sum+l[i+1]
-print(sum) 
+        c.append(l[i+1])
+print(sum(c)) 
