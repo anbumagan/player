2cda35a28a9722bbad36b788b87c1d90bb4c0f03
anbumagan <50615722+anbumagan@users.noreply.github.com>
2019-06-19 14:15:46 +0530

    Update p105.py

--- p105.py
+++ p105.py
@@ -1,6 +1,11 @@
 n=int(input())
 l=list(map(int,input().split()))
 s=[]
-for i in l:
-    s.append(l.index(i)+1)
-print(*s[::-1])
+if(l==sorted(l)):
+    for i in l:
+        s.append(l.index(i)+1)
+    print(*s)
+else:
+    for i in l:
+        s.append(l.index(i)+1)
+    print(*s[::-1]) 
