31c93f89ec50647d200f48a8890e3ae7c4af7103
anbumagan <50615722+anbumagan@users.noreply.github.com>
2019-06-19 15:39:26 +0530

    Update p111.py

--- p111.py
+++ p111.py
@@ -1,14 +1,17 @@
 n,m=map(int,input().split())
 l=list(map(int,input().split()))
 l1=l[:n]
-l2=l[m+1:]
+l2=[]
+for i in range(n,len(l)):
+    l2.append(l[i])
 c=[]
 if(len(l1)<len(l2)):
-    for i in l1:
-        if(i in l2):
-            c.append(i)
+    for j in l1:
+        if(j in l2):
+            c.append(j)
+    print(*sorted(c))
 else:
     for i in l2:
         if(i in l1):
             c.append(i)
-print(*sorted(set(c)))
+    print(*sorted(c))
