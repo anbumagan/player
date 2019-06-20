aaa081a04d705596d15912307c04adc85ebd4d16
anbumagan <50615722+anbumagan@users.noreply.github.com>
2019-06-20 14:04:53 +0530

    Create p122.py

---
+++ p122.py
@@ -0,0 +1,3 @@
+a=list(map(int,input().split("-")))
+l={1:"january",2:"febrauary",3:"march",4:"april",5:"may",6:"june",7:"july",8:"august",9:"september",10:"october",11:"november",12:"december"}
+print(l[a[1]].capitalize())
