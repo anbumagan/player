8458d4b8d07ee86a03a55033edc9c60a52456b48
anbumagan <50615722+anbumagan@users.noreply.github.com>
2019-06-20 13:05:46 +0530

    Update p118.py

--- p118.py
+++ p118.py
@@ -1,9 +1,2 @@
 a=list(map(str,input().split()))
-c=[]
-for i in range(len(a)-1):
-    if(len(a[i])==len(a[i+1])):
-        c.append(a[i])
-if(len(c)==0):
-    print(max(a))
-else:
-    print(c[0])
+print(max(a,key=len))
