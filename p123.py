e1dd199cd6fa43837db9d899981c50a1858dc0d4
anbumagan <50615722+anbumagan@users.noreply.github.com>
2019-06-20 14:19:24 +0530

    Update p123.py

--- p123.py
+++ p123.py
@@ -1,7 +1,7 @@
-a=int(input())
+a1=int(input())
 e=[]
-for i in range(2,a):
-    if(a%i==0):
+for i in range(2,a1):
+    if(a1%i==0):
         d=0
         for j in range(1,i+1):
             if(i%j==0):
