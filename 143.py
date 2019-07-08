0a2bba2896c460c3f6d66b35241903a2f2d3fe23
anbumagan <50615722+anbumagan@users.noreply.github.com>
2019-07-08 18:50:08 +0530

    Update 143.py

--- 143.py
+++ 143.py
@@ -1,9 +1,26 @@
-a1=int(input())
-a=list(map(int,input().split()))
-b=list(map(int,input().split()))
-c=[]
-for i in range(a1):
-    c.append([b[i],a[i]])
-c.sort()
-for i in c:
-    print(i[1],end=" ")
+import java.util.*;
+public class sample {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		HashMap<Integer, Integer> map=new HashMap<>();
+		int a1=scn.nextInt();
+		int[] a =new int[a1];
+		int[] b=new int[a1];
+		for(int i=0;i<a1;i++){
+		  a[i]=scn.nextInt(); 
+		}
+		for(int j=0;j<a1;j++) {
+			b[j]=scn.nextInt();
+		}
+		for(int i=0;i<a1;i++) {
+			map.put(b[i],a[i]);
+		}
+		TreeMap<Integer, Integer> tree =new TreeMap<>(map);
+		for(Map.Entry m:tree.entrySet()) {
+			System.out.print(m.getValue()+" ");
+		}
+
+	}
+
+}
